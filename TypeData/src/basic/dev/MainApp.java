85e59c9eb4a457130365ae1422147f6d980b950a
longla2222 <longla20092023@gmail.com>
2024-12-15 09:01:49 +0700

    Add files via upload

---
+++ TypeData/src/basic/dev/MainApp.java
@@ -0,0 +1,13 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short n = 120;
+		byte b = (byte) n;
+		System.out.println(b);
+
+	}
+
+}
