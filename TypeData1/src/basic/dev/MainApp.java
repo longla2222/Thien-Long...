53f39d21747b676561daaa4de33e7534b386a4e2
longla2222 <longla20092023@gmail.com>
2024-12-15 16:16:22 +0700

    Add files via upload

---
+++ TypeData1/src/basic/dev/MainApp.java
@@ -0,0 +1,45 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte b = 9;
+		System.out.println(b);
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.MAX_VALUE);
+
+		short s = 10;
+		System.out.println(s);
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.MAX_VALUE);
+
+		int i = 100;
+		System.out.println(b);
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.MAX_VALUE);
+		
+        long l = 100;
+		System.out.println(l);
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.MAX_VALUE);
+		
+		float f = 100;
+		System.out.println(f);
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.MAX_VALUE);
+		
+		double d = 100;
+		System.out.println(d);
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.MAX_VALUE);
+		
+		char c = 'A';
+		System.out.println(c);
+		
+		boolean bool = true;
+		System.out.println(bool);
+		
+	}
+
+}
